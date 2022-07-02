2baa1a54cd769055a9ae1af8ae64451c7735e023
abhuwania <abhuwania@zipcar.com>
2022-07-02 20:04:26 +0530

    add my files

--- kafka-producer-wikimedia/src/main/java/com/learn/springboot/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/learn/springboot/SpringBootProducerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootProducerApplication implements CommandLineRunner {
 
+
+    // Refer: https://youtube.com/playlist?list=PLGRDMO4rOGcNLwoack4ZiTyewUcF6y6BU
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
     }
